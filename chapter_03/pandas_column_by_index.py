3ba9a121d226634039315f3b3be95cfa4632f0e2
Lee HyunJoo <wayhome250@gmail.com>
2018-01-14 19:18:01 +0900

    chapter3 판다스 실습내용 추가

---
+++ chapter_03/pandas_column_by_index.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import pandas as pd
+import sys
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+
+data_frame = pd.read_excel(input_file, sheet_name='january_2013')
+
+data_frame_column_by_index = data_frame.iloc[:, [1, 4]]
+
+writer = pd.ExcelWriter(output_file)
+data_frame_column_by_index.to_excel(writer, sheet_name='jan_13_output', index=False)
+writer.save()
---
+++ chapter_03/pandas_column_by_name_all_worksheets.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import pandas as pd
+import sys
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+
+data_frame = pd.read_excel(input_file, sheet_name=None, index_col=None)
+
+row_output = []
+
+for worksheet_name, data in data_frame.items():
+    row_output.append(data.loc[:, ['Customer Name', 'Sale Amount']])
+filtered_rows = pd.concat(row_output, axis=0, ignore_index=True)
+
+writer = pd.ExcelWriter(output_file)
+filtered_rows.to_excel(writer, sheet_name='selected_columns_all_worksheets', index=False)
+writer.save()
